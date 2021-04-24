30660f76f2d19d72725654cc6e48139c4e47734d
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2021-04-24 15:55:18 +0100

    small changes.

--- egs/modified_yesno/asr_mfcc_char/run_group_piece.sh
+++ egs/modified_yesno/asr_mfcc_char/run_group_piece.sh
@@ -6,7 +6,7 @@ for dataset in 3gram; do
     train_set=train_${dataset}
     train_dev=dev_${dataset}
     for elayers in 1 2 3; do
-        for eunits in 10 20 30 40 50 60 70 80 90 100 110 120 130; do
+        for eunits in 30 40 50 60 70 80 90 100 110 120; do
             subtag=${elayers}_${eunits}_nosub
             ./run.sh --stage 1 \
                 --train-config ${train_config} \
