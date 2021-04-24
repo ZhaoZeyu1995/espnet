cbcc7a8de720752d6ac056b33eb2be551f01217b
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2021-04-24 21:22:13 +0100

    small changes

--- egs/modified_yesno/asr_mfcc_char/result_sum.sh
+++ egs/modified_yesno/asr_mfcc_char/result_sum.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-output_file="asr_mfcc_spk.result"
+output_file="exp/asr_mfcc_char.result"
 for train_set in train_yyn train_ynn train_3gram; do
     for nlayer in 1 2 3; do
-        for eunits in 10 20 30 40 50 60 70 80 90 100 110 120 130; do
-            expdir=exp/${train_set}_pytorch_train_nosub_delta_${nlayer}_${eunits}_nosub
+        for eunits in 30 40 50 60 70 80 90 100 110 120; do
+            expdir=exp/${train_set}_pytorch_train_delta_${nlayer}_${eunits}_nosub
             sum_wer=""
-            for test_set in test_yyn test_ynn test_3gram test_sam_yyn test_sam_ynn test_sam_3gram test_sam_yyn_noise test_sam_ynn_noise test_sam_3gram_noise; do
-                result_file=${expdir}/decode_${test_set}_decode/result.wrd.txt
+            for test_set in train_yyn train_ynn train_3gram test_yyn test_ynn test_3gram test_sam_yyn test_sam_ynn test_sam_3gram test_sam_yyn_noise test_sam_ynn_noise test_sam_3gram_noise; do
+                result_file=${expdir}/decode_${test_set}_decode/result.txt
                 wer=`cat ${result_file} | grep -m 1 "Mean" | awk '{print $11}'`
                 sum_wer="${sum_wer} ${wer}"
             done
